eddab8e7da2f2b63db086a0dd626c1267eda1b47
hartziai <arziai@gmail.com>
2016-06-15 15:50:50 -0700

    tuning logic given model and quality requirements

--- tuning.py
+++ tuning.py
@@ -1,9 +1,26 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.linear_model import Perceptron, LogisticRegression, BayesianRidge, SGDClassifier, \
+    PassiveAggressiveClassifier
+from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+# more information about sklearn's incremental models
+# http://scikit-learn.org/stable/auto_examples/linear_model/plot_sgd_comparison.html
+# http://scikit-learn.org/stable/modules/scaling_strategies.html
+# TODO add xgboost
+# TODO add other models
+models_linear = [Perceptron, LogisticRegression, BayesianRidge, LinearSVC]
+models_online = [Perceptron, MultinomialNB, SGDClassifier, PassiveAggressiveClassifier]
+models_nonlinear_cheap = [DecisionTreeClassifier]
+models_nonlinear_expensive = [RandomForestClassifier, SVC, GradientBoostingClassifier]
+
 
 def requirements_bare_minimum(y_train):
-    # model has to beat
+    # model has to beat:
     # 1- random guessing (AUC must be higher than 0.5), and
     # 2- majority vote classification, important for imbalanced datasets
     # 3- cross-validation variation should not be too high (less than 0.1)
@@ -13,7 +30,7 @@ def requirements_bare_minimum(y_train):
             'accuracy_std': 0.1}
 
 
-def tune(insights, x_train, x_test, y_train, y_test, requirements=None):
+def tune(insights, x_train, x_test, y_train, y_test, models='all', requirements=None):
     if requirements is not None:
         requirements = requirements_bare_minimum(y_train)
 
@@ -28,3 +45,20 @@ def tune(insights, x_train, x_test, y_train, y_test, requirements=None):
                         model_variation < requirements['accuracy_std'] and \
                         model_insights['auc'] > requirements['auc'] and \
                         model_insights['accuracy_test'] > requirements['accuracy']:
+            return model_name
+
+    # model selection and tuning loop
+    models_to_train = []
+
+    if models == 'all':
+        models_to_train += models_linear + models_nonlinear_cheap + models_nonlinear_expensive
+    elif models == 'linear':
+        models_to_train += models_online
+    elif models_to_train == 'cheap':
+        models_to_train += models_linear + models_nonlinear_cheap
+
+    for model in models_to_train:
+        # TODO: add the looping logic
+        pass
+
+    return None
